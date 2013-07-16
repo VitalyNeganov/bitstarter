b278b54500375b5faadb83af31d439f11912645f
Ubuntu <ubuntu@ip-172-31-46-168.us-west-2.compute.internal>
2013-07-16 12:27:17 +0000

    form file

--- web.js
+++ web.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var fileName = "ndex.html";
+
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+response.send(data);
+
+          console.log(data);
+          fs.close(fd);
+        });
+      });
+    });
+  }
+});
 });
 
 var port = process.env.PORT || 5000;
